ad6243ac050a6603a3b1d8177cecf740072b3674
Leonid Shkolnyk <leonid@itsyndicate.org>
2018-05-23 09:50:40 +0300

    Start 3 lab

--- app/src/main/java/com/example/amenight/univer/MainActivity.java
+++ app/src/main/java/com/example/amenight/univer/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.amenight.univer;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.amenight.univer.databinding.ActivityMainBinding;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.Scheduler;
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.btnClickMe.setOnClickListener(this::onClickMeClick);
+    }
+
+    private void onClickMeClick(View view) {
+        binding.pbProgress.setVisibility(View.VISIBLE);
+        Observable.timer(3000, MILLISECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(aLong -> binding.pbProgress.setVisibility(View.GONE),
+                        Throwable::printStackTrace);
     }
 }
